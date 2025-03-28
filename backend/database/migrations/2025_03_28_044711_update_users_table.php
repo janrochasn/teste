af5ef1e384cdd4c5aadb972a9e0920d43aac14ab
Janderson Rocha <janderson.rocha97@gmail.com>
2025-03-28 01:50:24 -0300

    Criação das migrations

---
+++ backend/database/migrations/2025_03_28_044711_update_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('email_verified_at');
+            $table->dropColumn('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function ($table) {
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('remember_token')->str_random(10);
+        });
+    }
+};
---
+++ backend/database/migrations/2025_03_28_044829_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->float('price');
+            $table->bigInteger('categories_id')->unsigned();
+            $table->foreign('categories_id')->references('id')->on('categories');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['categories_id']);
+            $table->dropColumn(['categories_id']);
+        });
+        
+        Schema::dropIfExists('products');
+    }
+};
